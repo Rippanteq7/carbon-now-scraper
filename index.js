3030807f793983fd156a4693273cd47dee43836d
Rippanteq7 <104727253+Rippanteq7@users.noreply.github.com>
2024-08-08 13:06:15 +0700

    refactor

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const puppeteer = require("puppeteer");
-const fs = require("fs-extra");
-const path = require("path");
-
 const { options, themes, langs, fonts } = require("./options.js");
-
-/**
- * @param {Object} obj - a Array for Convert to Parameter
- */
-function createParameter(obj) {
-  const parameter = Object.entries(obj).map(
-    ([key, value]) => `${key}=${encodeURIComponent(value)}`
-  );
-
-  return parameter.join("&");
-}
 
 /**
  *
  * @param {String} code - a Program code for Carbon
  * @param {String} outputPath - Output File Name Path
  * @param {Object} option - a additional Argument Option for Carbon
- * @returns
+ * @returns {String} outputPath
  */
 async function carbon(code, outputPath, option = {}) {
   const {
@@ -36,25 +22,20 @@ async function carbon(code, outputPath, option = {}) {
   } = { ...options, ...option };
 
   // Checking
-  if (typeof code === "undefined")
-    throw new Error("Program Code argument cannot be empty!");
-  if (!langs.includes(lang))
-    throw new Error(
-      `There is no ${lang} programming language, please check at carbon.now.sh for a list of programming languages`
-    );
-  if (!themes.includes(theme))
-    throw new Error(
-      `There is no ${theme} Carbon Themes, please check at carbon.now.sh for a list of Theme`
-    );
-  if (!fonts.includes(font))
-    throw new Error(
-      `There is no ${font} Carbon Fonts, please check at carbon.now.sh for a list of Fonts`
-    );
+  let msg = null
+  if (typeof code === "undefined") msg = "Program Code argument cannot be empty!";
+  if (!langs.includes(lang)) msg = `There is no ${lang} programming language, `;
+    + `please check at carbon.now.sh for a list of programming languages`;
+  if (!themes.includes(theme)) msg = `There is no ${theme} Carbon Themes, `;
+    + `please check at carbon.now.sh for a list of Theme`;
+  if (!fonts.includes(font)) msg = `There is no ${font} Carbon Fonts, `;
+    + `please check at carbon.now.sh for a list of Fonts`;
+  if (msg) throw new Error(msg)
 
   let fontParam = font.replace("-", " ");
   // Parameter Url
-  let parameter = {
-    code: code,
+  let parameter = new URLSearchParams({
+    code,
     l: lang,
     bg: background,
     t: theme,
@@ -64,8 +45,8 @@ async function carbon(code, outputPath, option = {}) {
     ln: line,
     fl: firstLine,
     wm: watermark,
-  };
-  let url = "https://carbon.now.sh?" + createParameter(parameter);
+  }):
+  let url = "https://carbon.now.sh?" + parameter.toString();
 
   return openBrowser(url, outputPath, option.puppeteer);
 }
@@ -74,73 +55,38 @@ async function carbon(code, outputPath, option = {}) {
  *
  * @param {String} url - Url for Downloading
  * @param {String} outputPath - Output for File
+ * @param {Object} optsPuppeteer - Options for puppeteer launch
  */
 async function openBrowser(url, outputPath, optsPuppeteer = {}) {
-  // Parse Output Folder
-  let output = path.resolve(outputPath);
-  let folder = path.dirname(output);
-  let filename = path.basename(output);
-  if (!path.extname(filename)) {
-    folder = output;
-    filename = "Downloaded.png";
-  }
-
   // Start Puppeteer Session
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: "new", ...optsPuppeteer
+    headless: "new",
+    ...optsPuppeteer,
   });
 
-  // Open Page and Go to Carbon Site
-  const page = await browser.newPage();
-  await page.goto(url);
+  try {
+    // Open Page and Go to Carbon Site
+    const page = await browser.newPage();
+    await page.goto(url);
+    // Make Downloaded file more HD
+    await page.setViewport({
+    	width: 1920,
+        height: 1080,
+        deviceScaleFactor: 2,
+    });
+    // Screenshot the element
+    await page.waitForSelector("#export-container");
+    const element = await page.$("#export-container");
+    await element.screenshot({ path: outputPath });
 
-  // Make Downloaded file more HD
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-    deviceScaleFactor: 2,
-  });
-
-  // Download
-  let downloaded = null;
-
-  // Event
-  let client = await page.target().createCDPSession();
-  await client.send("Browser.setDownloadBehavior", {
-    behavior: "allowAndName",
-    downloadPath: folder,
-    eventsEnabled: true,
-  });
-
-  client.on("Browser.downloadProgress", async (event) => {
-    // Identify if File Downloaded
-    if (event.state === "completed") {
-      let newfilename = path.resolve(folder, filename)
-      fs.renameSync(
-        path.resolve(folder, event.guid),
-        newfilename
-      );
-
-      // Close Browser after Success Download
-      await browser.close();
-      downloaded = newfilename
-    }
-  });
-
-  // Click Export in Carbon.now.sh Site
-  await page.waitForSelector(".editor");
-  await page.waitForSelector(".jsx-2184717013");
-  await page.click(".jsx-2184717013");
-
-  return new Promise(async (resolve, reject) => {
-    let checkDownloaded = setInterval(() => {
-      if (downloaded !== null) {
-        clearInterval(checkDownloaded);
-        resolve(downloaded);
-      }
-    }, 100);
-  });
+    return outputPath;
+  } catch (e) {
+    throw new Error(e)
+  } finally {
+    // Close Browser
+    await browser.close()
+  }
 }
 
 module.exports = carbon;
